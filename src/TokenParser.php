ab04dd852215eb7cc403ec433bd6ab9aa18ec77b
janoschreuschenbach <j.reuschenbach@onofice.com>
2020-04-25 20:39:54 +0200

    TokenParser unterstützt jetzt listener

--- src/TokenParser.php
+++ src/TokenParser.php
@@ -7,8 +7,35 @@ class TokenParser
     /** @var Tokens */
     private $_pTokens = null;
 
+    /** @var array */
+    private $_listener = [];
+
     public function __construct(Tokens $pTokenizer)
     {
         $this->_pTokens = $pTokenizer;
+    }
+
+    public function addListener($tokenIndex, $callback)
+    {
+        $this->_listener[$tokenIndex] []= $callback;
+    }
+
+    public function parse()
+    {
+        foreach ($this->_pTokens as $token)
+        {
+            if (is_array($token) && array_key_exists($token[0], $this->_listener))
+            {
+                $this->callListener($this->_listener[$token[0]]);
+            }
+        }
+    }
+
+    private function callListener(array $listeners)
+    {
+        foreach ($listeners as $listener)
+        {
+            call_user_func($listener);
+        }
     }
 }
--- tests/TokenParserTest.php
+++ tests/TokenParserTest.php
@@ -6,9 +6,26 @@ use jr\countif\Tokens;
 
 class TokenParserTest extends TestCase
 {
+    private $_dummyListenerCalled = 0;
+
     public function testInstance(): void
     {
         $pTokens = new Tokens(array());
         $this->assertInstanceOf(TokenParser::class, new TokenParser($pTokens));
+    }
+
+    public function testListener()
+    {
+        $pTokens = new Tokens(array(array(T_IF, 'if', 1)));
+        $pTokenParser = new TokenParser($pTokens);
+        $pTokenParser->addListener(T_IF, array($this, 'dummyListener'));
+        $pTokenParser->parse();
+
+        $this->assertEquals(1, $this->_dummyListenerCalled);
+    }
+
+    public function dummyListener()
+    {
+        $this->_dummyListenerCalled++;
     }
 }
